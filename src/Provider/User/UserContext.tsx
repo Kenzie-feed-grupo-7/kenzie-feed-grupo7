23aa39bc4b45acb41a542b9b6c289bbba3b2e8b6
christianeyamate <chtistiane.osiro@gmail.com>
2023-07-07 22:11:07 -0300

    atualizando projeto final

--- src/Provider/User/UserContext.tsx
+++ src/Provider/User/UserContext.tsx
@@ -1,7 +1,7 @@
 import { createContext, useEffect, useState } from "react";
 import { api } from "../../Services/api";
 import { IUserContext, IUserProviderProps, IUser, IUserRegisterResponse, IUserLoginResponse } from "./@types";
-import { useLocation, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { TRegisterForm } from "../../Pages/Register/registerFormSchema";
 import { toast } from "react-toastify";
 import { TLoginForm } from "../../Pages/Login/loginFormSchema";
